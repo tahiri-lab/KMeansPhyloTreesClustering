c153157a07af8b198defa5dd88467a98540ab5e8
Adio Yaya <yaya.mantinou_adio@courrier.uqam.ca>
2026-03-09 22:42:40 -0400

    fix:éviter les appels snprintf à format vide dans readParameters()

--- src/fonctions.cpp
+++ src/fonctions.cpp
@@ -467,8 +467,9 @@ int readParameters(struct Parameters * param){
     snprintf((*param).version, sizeof((*param).version), "consol");
     snprintf((*param).multiple, sizeof((*param).multiple), "no");
     snprintf((*param).multigene, sizeof((*param).multigene), "no");
-    snprintf((*param).path, sizeof((*param).path), "");
-    snprintf(input, sizeof(input), "");
+    /* avoid zero-length format string warnings by assigning empty strings directly */
+    (*param).path[0] = '\0';
+    input[0] = '\0';
     snprintf(output, sizeof(output), "output.txt");
     snprintf(hgtResultFile, sizeof(hgtResultFile), "hgtresultfile.txt");
     snprintf((*param).scenario, sizeof((*param).scenario), "unique");
